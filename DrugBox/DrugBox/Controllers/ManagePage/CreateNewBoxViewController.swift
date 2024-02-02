5e0decd2b5b7b7ea03025423c8bc04476f267a2d
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-02-02 17:43:17 +0900

    Feat: 구급상자 이미지 로컬에서 가져오기 구현
    - 구급상자 생성 UI 일부 수정: 기존에는 버튼만 -> imageview & button 으로 불러온 이미지를 띄워줌

--- DrugBox/DrugBox/Controllers/ManagePage/CreateNewBoxViewController.swift
+++ DrugBox/DrugBox/Controllers/ManagePage/CreateNewBoxViewController.swift
@@ -8,14 +8,63 @@
 import UIKit
 
 class CreateNewBoxViewController: UIViewController {
-    @IBOutlet weak var ImagePickButton: UIButton!
+    @IBOutlet weak var ImageView: UIImageView!
     @IBOutlet weak var BoxNameLabel: UITextField!
     @IBOutlet weak var BoxCodeLabel: UITextField!
     @IBOutlet weak var SaveButton: UIButton!
+    @IBOutlet weak var ImagePickButton: UIButton!
+    
+    let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func pickButtonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "사진을 가져올 곳 선택", message: "", preferredStyle: .actionSheet)
+        let library = UIAlertAction(title: "사진앨범", style: .default) {
+            (action) in self.openLibrary()
+        }
+        let camera = UIAlertAction(title: "카메라", style: .default) {
+            (action) in self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+}
+
+extension CreateNewBoxViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func openLibrary() {
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func openCamera() {
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            present(picker, animated: false, completion: nil)
+        } else {
+            print("Camera is not available")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            ImageView.image = image
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
